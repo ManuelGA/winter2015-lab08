823532987a9bd9820c8fade429c4da0ce472e2a0
Manuel Gonzales <manuel.gonzales@shaw.ca>
2015-04-09 20:58:10 -0700

    Halfway done
    Signed-off-by:Manuel Gonzales <manuel.gonzales@shaw.ca>

---
+++ application/controllers/Auth.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Auth extends Application 
+{
+  function __construct()
+  {
+    parent::__construct();
+    $this->load->helper('url');
+  }
+  function index()
+  {
+    $this->data['pagebody'] = 'login';
+    $this->render();
+  }
+}
